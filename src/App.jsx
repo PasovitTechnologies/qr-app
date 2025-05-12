fd0b588eeda6e2a6a004cc38ceaea9a009e4e7f8
PasovitTechnologies <tech.admin@eafo.info>
2025-05-12 15:07:12 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import QRScanner from './components/QRScanner';
@@ -6,14 +6,20 @@ import QRView from './components/QRView';
 import Navbar from './components/Navbar';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-      <Navbar/>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/qrscanner" element={<QRScanner />} />
-      <Route path="/qrscanner/view/:userId/:courseId/:formId" element={<QRView />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login onLogin={handleLogin} />} />
+        <Route path="/qrscanner" element={<QRScanner />} />
+        <Route path="/qrscanner/view/:userId/:courseId/:formId" element={<QRView />} />
+      </Routes>
     </div>
   );
 };
